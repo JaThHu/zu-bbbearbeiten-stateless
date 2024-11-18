27bcc33ef2b6466112ccc8edc55f6e071f3fcfb6
JaThHu <janickhurschler06@gmail.com>
2024-11-18 14:21:49 +0100

    Asks the user for date, closes #1

--- helper.py
+++ helper.py
@@ -1,26 +1,32 @@
 from dataclasses import dataclass
+import datetime
 
+# Liste der To-dos
 items = []
-
 
 @dataclass
 class Item:
     text: str
+    date: datetime.date
     isCompleted: bool = False
 
-
-def add(text):
+def add(text, date):
+    # Datum in datetime.date umwandeln
+    parsed_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    
+    # Text anpassen und Item erstellen
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
-
+    new_item = Item(text=text, date=parsed_date)
+    
+    # Item zur Liste hinzufügen und sortieren
+    items.append(new_item)
+    items.sort(key=lambda x: x.date)
 
 def get_all():
     return items
 
-
 def get(index):
     return items[index]
-
 
 def update(index):
     items[index].isCompleted = not items[index].isCompleted
